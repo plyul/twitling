47cba82bb7ad03972a3d0699210ddd50b9185ed0
Павел Люльченко <graygolem@gmail.com>
2021-01-26 22:28:17 +0500

    Добавлена рыба gRPC

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,48 +1,32 @@
 package main
 
 import (
-	"encoding/hex"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
 	"log"
-	"twitling/generated/model"
+	"twitling/generated/api"
 )
 
-const userId = 1 //nolint
-
-var texts = [...]string{
-	"Hello there. Thanks for the follow. Did you notice, that I am an egg? A talking egg? Damn!",
-	"Thanks mate! Feel way better now",
-	"Yeah that is crazy",
-	"Hi",
-	"Thanks",
-	"Okay",
-	"I just wrote 'protobug' instead of 'protobuf' in a report, it's a good summary of Monday morning.",
-}
+const (
+	apiAddress = "localhost"
+	apiPort = 1025
+)
 
 func main() {
-	var jl, pl float64
-	for account, text := range texts {
-		m := model.PostDTO{
-			Text: text,
-			ReplyTo: 0,
-			ToUser: int64(account),
-			PublishAt: nil,
-		}
-		protobufString, err := proto.Marshal(&m)
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-		s := hex.EncodeToString(protobufString)
-		jsonString, err := protojson.Marshal(&m)
-		if err != nil {
-			log.Fatalf("%v", err)
-		}
-		pl += float64(len(protobufString))
-		log.Printf("Message(hex, len=%d)=%s", len(protobufString), s)
-		jl += float64(len(jsonString))
-		log.Printf("Message(json, len=%d)=%s", len(jsonString), jsonString)
-		log.Printf("Protobuf efficiency for message: %.2f", float64(len(protobufString))/float64(len(jsonString)))
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", apiAddress, apiPort), grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("error connecting to server: %v", err)
 	}
-	log.Printf("Cumulative protobuf efficiency: %.2f", pl/jl)
+	defer func() {
+		if err := conn.Close(); err != nil {
+			log.Fatalf("error closing server connection: %v", err)
+		}
+	}()
+	client := api.NewMessagingServiceClient(conn)
+	response, err := client.Ping(context.Background(), &api.PingRequest{Challenge: "ping"})
+	if err != nil {
+		log.Fatalf("error calling Ping method: %v", err)
+	}
+	fmt.Printf("Got response: %s", response.Response)
 }
